02502b88f8c4be62e2799852730104f5a0fbe8d3
KarthusLorin <karthuslorin@gmail.com>
2019-05-22 23:04:25 +0800

    transform params to url

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import {AxiosRequestConfig} from "./types";
 import xhr from './xhr'
+import {buildURL} from "./helpers/url";
 
 function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
   xhr(config)
+}
+
+// 处理（规范化，序列化）配置
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+}
+
+// 序列化参数
+function transformURL(config: AxiosRequestConfig): string {
+  const {url, params} = config
+  return buildURL(url, params)
 }
 
 export default axios
